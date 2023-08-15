cbfabbcb5dd8ab18e5fb13d7a7989e49e0efb8da
Saqueeb Begumpure <saqueebbegumpure@gmail.com>
2023-08-15 14:22:20 +0530

    Natural number

---
+++ natural number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    int n;
+    printf("Enter the value of n: \n");
+    scanf("%d", &n);
+    
+    do {
+        printf("the number is %d \n", i+1);
+        i++;
+    } while (i <= n);
+    
+    return 0;
+}
